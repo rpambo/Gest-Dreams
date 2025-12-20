79b58b867bca91da1984ad23b1258049a7c53bdd
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-12-20 23:44:45 +0100

    seccao founder adicionado

--- gest-dreams/src/app/pagina/home/home.ts
+++ gest-dreams/src/app/pagina/home/home.ts
@@ -6,10 +6,11 @@ import { Impacto } from "../../components/impacto/impacto";
 import { Servicos } from "../../components/servicos/servicos";
 import { VamosComecar } from "../../components/vamos-comecar/vamos-comecar";
 import { Inovacao } from "../../components/inovacao/inovacao";
+import { Founder } from "../../components/founder/founder";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Slides, QuemSomos, Impacto, Servicos, VamosComecar, Inovacao],
+  imports: [Navbar, Slides, QuemSomos, Impacto, Servicos, VamosComecar, Inovacao, Founder],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
